0d62f418a752a958e6531a58341767cc10542a72
moeketsi <raymondkxng17@gmail.com>
2023-02-04 20:14:36 +0200

    mobile nav animations

---
+++ app.js
@@ -0,0 +1,15 @@
+const button = document.querySelector(".burger");
+const navlinks = document.querySelector('.navlinks');
+const logo = document.querySelector('.logo');
+const nav = document.querySelector('nav');
+
+
+button.addEventListener("click", () => {
+    button.classList.toggle("active");
+    navlinks.classList.toggle("open")
+    logo.classList.toggle("active")
+    nav.classList.toggle("active")
+
+
+
+});
