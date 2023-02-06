c4ebb57357c160cb583410a49eb840b53027a743
moeketsi <raymondkxng17@gmail.com>
2023-02-06 12:38:07 +0200

    available works

--- slider.js
+++ slider.js
@@ -1,4 +1,4 @@
-import Frame from 'https://cdn.jsdelivr.net/gh/ofekN/Frame/src/frame.js'
+// import Frame from 'https://cdn.jsdelivr.net/gh/ofekN/Frame/src/frame.js'
 
 let num = 0
 let slides = [...document.querySelectorAll('.slide')]
@@ -16,10 +16,10 @@ if(n===0)
   }
   else
     {
-slides[n].style.left = '100%'
-slides[n-1].style.left = '0%'
- Frame.run(slides[n-1],{left:'-100%',duration:.5})
- Frame.run(slides[n],{left:'0px',duration:.5})
+    slides[n].style.left = '100%'
+    slides[n-1].style.left = '0%'
+      Frame.run(slides[n-1],{left:'-100%',duration:.5})
+      Frame.run(slides[n],{left:'0px',duration:.5})
 
     }
 
@@ -41,7 +41,7 @@ function moveSlidePrev(n){
     }
   else if(n === slides.length - 1)
     {
-     slides[n].style.left = '-100%'
+      slides[n].style.left = '-100%'
       slides[0].style.left = '0px'
 
       Frame.run(slides[0],{left:'100%',duration:.5})
